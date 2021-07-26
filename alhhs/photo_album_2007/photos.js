a2c4308929c737de27460d7bb25ce9114adce427
moonflea <pinky@library.ucla.edu>
2021-07-26 15:43:57 -0700

    ALHHS biomed site first upload to Github

---
+++ alhhs/photo_album_2007/photos.js
@@ -0,0 +1,352 @@
+WPGgallery = new Object();
+WPGgallery.name = "2007%20ALHHS%20Meeting%20-%20Montreal%2C%20Qu%E9bec";
+WPGgallery.photographer = "";
+WPGgallery.contact = "";
+WPGgallery.email = "";
+WPGgallery.date = "7/23/2007"
+
+WPGgallery.colors = new Object();
+WPGgallery.colors.background = "#FFFFFF";
+WPGgallery.colors.banner = "#F0F0F0";
+WPGgallery.colors.text = "#000000";
+WPGgallery.colors.link = "#0000FF";
+WPGgallery.colors.alink = "#FF0000";
+WPGgallery.colors.vlink = "#800080";
+
+gPhotos = new Array();
+gPhotos[0] = new Object();
+gPhotos[0].filename = "IMG_0161.jpg";
+gPhotos[0].ImageWidth = 600;
+gPhotos[0].ImageHeight = 450;
+gPhotos[0].ThumbWidth = 75;
+gPhotos[0].ThumbHeight = 56;
+gPhotos[0].meta = new Object();
+
+gPhotos[1] = new Object();
+gPhotos[1].filename = "IMG_0162.jpg";
+gPhotos[1].ImageWidth = 600;
+gPhotos[1].ImageHeight = 450;
+gPhotos[1].ThumbWidth = 75;
+gPhotos[1].ThumbHeight = 56;
+gPhotos[1].meta = new Object();
+
+gPhotos[2] = new Object();
+gPhotos[2].filename = "IMG_0163.jpg";
+gPhotos[2].ImageWidth = 600;
+gPhotos[2].ImageHeight = 450;
+gPhotos[2].ThumbWidth = 75;
+gPhotos[2].ThumbHeight = 56;
+gPhotos[2].meta = new Object();
+
+gPhotos[3] = new Object();
+gPhotos[3].filename = "IMG_0164.jpg";
+gPhotos[3].ImageWidth = 600;
+gPhotos[3].ImageHeight = 450;
+gPhotos[3].ThumbWidth = 75;
+gPhotos[3].ThumbHeight = 56;
+gPhotos[3].meta = new Object();
+
+gPhotos[4] = new Object();
+gPhotos[4].filename = "IMG_0165.jpg";
+gPhotos[4].ImageWidth = 600;
+gPhotos[4].ImageHeight = 450;
+gPhotos[4].ThumbWidth = 75;
+gPhotos[4].ThumbHeight = 56;
+gPhotos[4].meta = new Object();
+
+gPhotos[5] = new Object();
+gPhotos[5].filename = "IMG_0166.jpg";
+gPhotos[5].ImageWidth = 600;
+gPhotos[5].ImageHeight = 450;
+gPhotos[5].ThumbWidth = 75;
+gPhotos[5].ThumbHeight = 56;
+gPhotos[5].meta = new Object();
+
+gPhotos[6] = new Object();
+gPhotos[6].filename = "IMG_0167.jpg";
+gPhotos[6].ImageWidth = 600;
+gPhotos[6].ImageHeight = 450;
+gPhotos[6].ThumbWidth = 75;
+gPhotos[6].ThumbHeight = 56;
+gPhotos[6].meta = new Object();
+
+gPhotos[7] = new Object();
+gPhotos[7].filename = "IMG_0168.jpg";
+gPhotos[7].ImageWidth = 600;
+gPhotos[7].ImageHeight = 450;
+gPhotos[7].ThumbWidth = 75;
+gPhotos[7].ThumbHeight = 56;
+gPhotos[7].meta = new Object();
+
+gPhotos[8] = new Object();
+gPhotos[8].filename = "IMG_0169.jpg";
+gPhotos[8].ImageWidth = 600;
+gPhotos[8].ImageHeight = 450;
+gPhotos[8].ThumbWidth = 75;
+gPhotos[8].ThumbHeight = 56;
+gPhotos[8].meta = new Object();
+
+gPhotos[9] = new Object();
+gPhotos[9].filename = "IMG_0170.jpg";
+gPhotos[9].ImageWidth = 600;
+gPhotos[9].ImageHeight = 450;
+gPhotos[9].ThumbWidth = 75;
+gPhotos[9].ThumbHeight = 56;
+gPhotos[9].meta = new Object();
+
+gPhotos[10] = new Object();
+gPhotos[10].filename = "IMG_0171.jpg";
+gPhotos[10].ImageWidth = 600;
+gPhotos[10].ImageHeight = 450;
+gPhotos[10].ThumbWidth = 75;
+gPhotos[10].ThumbHeight = 56;
+gPhotos[10].meta = new Object();
+
+gPhotos[11] = new Object();
+gPhotos[11].filename = "IMG_0172.jpg";
+gPhotos[11].ImageWidth = 600;
+gPhotos[11].ImageHeight = 450;
+gPhotos[11].ThumbWidth = 75;
+gPhotos[11].ThumbHeight = 56;
+gPhotos[11].meta = new Object();
+
+gPhotos[12] = new Object();
+gPhotos[12].filename = "IMG_0173.jpg";
+gPhotos[12].ImageWidth = 600;
+gPhotos[12].ImageHeight = 450;
+gPhotos[12].ThumbWidth = 75;
+gPhotos[12].ThumbHeight = 56;
+gPhotos[12].meta = new Object();
+
+gPhotos[13] = new Object();
+gPhotos[13].filename = "IMG_0174.jpg";
+gPhotos[13].ImageWidth = 600;
+gPhotos[13].ImageHeight = 450;
+gPhotos[13].ThumbWidth = 75;
+gPhotos[13].ThumbHeight = 56;
+gPhotos[13].meta = new Object();
+
+gPhotos[14] = new Object();
+gPhotos[14].filename = "IMG_0175.jpg";
+gPhotos[14].ImageWidth = 600;
+gPhotos[14].ImageHeight = 450;
+gPhotos[14].ThumbWidth = 75;
+gPhotos[14].ThumbHeight = 56;
+gPhotos[14].meta = new Object();
+
+gPhotos[15] = new Object();
+gPhotos[15].filename = "IMG_0176.jpg";
+gPhotos[15].ImageWidth = 600;
+gPhotos[15].ImageHeight = 450;
+gPhotos[15].ThumbWidth = 75;
+gPhotos[15].ThumbHeight = 56;
+gPhotos[15].meta = new Object();
+
+gPhotos[16] = new Object();
+gPhotos[16].filename = "IMG_0177.jpg";
+gPhotos[16].ImageWidth = 600;
+gPhotos[16].ImageHeight = 450;
+gPhotos[16].ThumbWidth = 75;
+gPhotos[16].ThumbHeight = 56;
+gPhotos[16].meta = new Object();
+
+gPhotos[17] = new Object();
+gPhotos[17].filename = "IMG_0178.jpg";
+gPhotos[17].ImageWidth = 600;
+gPhotos[17].ImageHeight = 450;
+gPhotos[17].ThumbWidth = 75;
+gPhotos[17].ThumbHeight = 56;
+gPhotos[17].meta = new Object();
+
+gPhotos[18] = new Object();
+gPhotos[18].filename = "IMG_0179.jpg";
+gPhotos[18].ImageWidth = 600;
+gPhotos[18].ImageHeight = 450;
+gPhotos[18].ThumbWidth = 75;
+gPhotos[18].ThumbHeight = 56;
+gPhotos[18].meta = new Object();
+
+gPhotos[19] = new Object();
+gPhotos[19].filename = "IMG_0180.jpg";
+gPhotos[19].ImageWidth = 600;
+gPhotos[19].ImageHeight = 450;
+gPhotos[19].ThumbWidth = 75;
+gPhotos[19].ThumbHeight = 56;
+gPhotos[19].meta = new Object();
+
+gPhotos[20] = new Object();
+gPhotos[20].filename = "IMG_0181.jpg";
+gPhotos[20].ImageWidth = 600;
+gPhotos[20].ImageHeight = 450;
+gPhotos[20].ThumbWidth = 75;
+gPhotos[20].ThumbHeight = 56;
+gPhotos[20].meta = new Object();
+
+gPhotos[21] = new Object();
+gPhotos[21].filename = "IMG_0182.jpg";
+gPhotos[21].ImageWidth = 600;
+gPhotos[21].ImageHeight = 450;
+gPhotos[21].ThumbWidth = 75;
+gPhotos[21].ThumbHeight = 56;
+gPhotos[21].meta = new Object();
+
+gPhotos[22] = new Object();
+gPhotos[22].filename = "IMG_0183.jpg";
+gPhotos[22].ImageWidth = 600;
+gPhotos[22].ImageHeight = 450;
+gPhotos[22].ThumbWidth = 75;
+gPhotos[22].ThumbHeight = 56;
+gPhotos[22].meta = new Object();
+
+gPhotos[23] = new Object();
+gPhotos[23].filename = "IMG_0184.jpg";
+gPhotos[23].ImageWidth = 600;
+gPhotos[23].ImageHeight = 450;
+gPhotos[23].ThumbWidth = 75;
+gPhotos[23].ThumbHeight = 56;
+gPhotos[23].meta = new Object();
+
+gPhotos[24] = new Object();
+gPhotos[24].filename = "IMG_0185.jpg";
+gPhotos[24].ImageWidth = 600;
+gPhotos[24].ImageHeight = 450;
+gPhotos[24].ThumbWidth = 75;
+gPhotos[24].ThumbHeight = 56;
+gPhotos[24].meta = new Object();
+
+gPhotos[25] = new Object();
+gPhotos[25].filename = "IMG_0187.jpg";
+gPhotos[25].ImageWidth = 600;
+gPhotos[25].ImageHeight = 450;
+gPhotos[25].ThumbWidth = 75;
+gPhotos[25].ThumbHeight = 56;
+gPhotos[25].meta = new Object();
+
+gPhotos[26] = new Object();
+gPhotos[26].filename = "IMG_0188.jpg";
+gPhotos[26].ImageWidth = 600;
+gPhotos[26].ImageHeight = 450;
+gPhotos[26].ThumbWidth = 75;
+gPhotos[26].ThumbHeight = 56;
+gPhotos[26].meta = new Object();
+
+gPhotos[27] = new Object();
+gPhotos[27].filename = "IMG_0189.jpg";
+gPhotos[27].ImageWidth = 450;
+gPhotos[27].ImageHeight = 600;
+gPhotos[27].ThumbWidth = 56;
+gPhotos[27].ThumbHeight = 75;
+gPhotos[27].meta = new Object();
+
+gPhotos[28] = new Object();
+gPhotos[28].filename = "IMG_0190.jpg";
+gPhotos[28].ImageWidth = 450;
+gPhotos[28].ImageHeight = 600;
+gPhotos[28].ThumbWidth = 56;
+gPhotos[28].ThumbHeight = 75;
+gPhotos[28].meta = new Object();
+
+gPhotos[29] = new Object();
+gPhotos[29].filename = "IMG_0191.jpg";
+gPhotos[29].ImageWidth = 600;
+gPhotos[29].ImageHeight = 450;
+gPhotos[29].ThumbWidth = 75;
+gPhotos[29].ThumbHeight = 56;
+gPhotos[29].meta = new Object();
+
+gPhotos[30] = new Object();
+gPhotos[30].filename = "IMG_0192.jpg";
+gPhotos[30].ImageWidth = 450;
+gPhotos[30].ImageHeight = 600;
+gPhotos[30].ThumbWidth = 56;
+gPhotos[30].ThumbHeight = 75;
+gPhotos[30].meta = new Object();
+
+gPhotos[31] = new Object();
+gPhotos[31].filename = "IMG_0194.jpg";
+gPhotos[31].ImageWidth = 600;
+gPhotos[31].ImageHeight = 450;
+gPhotos[31].ThumbWidth = 75;
+gPhotos[31].ThumbHeight = 56;
+gPhotos[31].meta = new Object();
+
+gPhotos[32] = new Object();
+gPhotos[32].filename = "IMG_0195.jpg";
+gPhotos[32].ImageWidth = 600;
+gPhotos[32].ImageHeight = 450;
+gPhotos[32].ThumbWidth = 75;
+gPhotos[32].ThumbHeight = 56;
+gPhotos[32].meta = new Object();
+
+gPhotos[33] = new Object();
+gPhotos[33].filename = "IMG_0197.jpg";
+gPhotos[33].ImageWidth = 450;
+gPhotos[33].ImageHeight = 600;
+gPhotos[33].ThumbWidth = 56;
+gPhotos[33].ThumbHeight = 75;
+gPhotos[33].meta = new Object();
+
+gPhotos[34] = new Object();
+gPhotos[34].filename = "IMG_0198.jpg";
+gPhotos[34].ImageWidth = 600;
+gPhotos[34].ImageHeight = 450;
+gPhotos[34].ThumbWidth = 75;
+gPhotos[34].ThumbHeight = 56;
+gPhotos[34].meta = new Object();
+
+gPhotos[35] = new Object();
+gPhotos[35].filename = "IMG_0200.jpg";
+gPhotos[35].ImageWidth = 600;
+gPhotos[35].ImageHeight = 450;
+gPhotos[35].ThumbWidth = 75;
+gPhotos[35].ThumbHeight = 56;
+gPhotos[35].meta = new Object();
+
+gPhotos[36] = new Object();
+gPhotos[36].filename = "IMG_0202.jpg";
+gPhotos[36].ImageWidth = 600;
+gPhotos[36].ImageHeight = 450;
+gPhotos[36].ThumbWidth = 75;
+gPhotos[36].ThumbHeight = 56;
+gPhotos[36].meta = new Object();
+
+gPhotos[37] = new Object();
+gPhotos[37].filename = "IMG_0203.jpg";
+gPhotos[37].ImageWidth = 600;
+gPhotos[37].ImageHeight = 450;
+gPhotos[37].ThumbWidth = 75;
+gPhotos[37].ThumbHeight = 56;
+gPhotos[37].meta = new Object();
+
+gPhotos[38] = new Object();
+gPhotos[38].filename = "IMG_0204.jpg";
+gPhotos[38].ImageWidth = 600;
+gPhotos[38].ImageHeight = 450;
+gPhotos[38].ThumbWidth = 75;
+gPhotos[38].ThumbHeight = 56;
+gPhotos[38].meta = new Object();
+
+gPhotos[39] = new Object();
+gPhotos[39].filename = "IMG_0205.jpg";
+gPhotos[39].ImageWidth = 450;
+gPhotos[39].ImageHeight = 600;
+gPhotos[39].ThumbWidth = 56;
+gPhotos[39].ThumbHeight = 75;
+gPhotos[39].meta = new Object();
+
+gPhotos[40] = new Object();
+gPhotos[40].filename = "IMG_0206.jpg";
+gPhotos[40].ImageWidth = 600;
+gPhotos[40].ImageHeight = 450;
+gPhotos[40].ThumbWidth = 75;
+gPhotos[40].ThumbHeight = 56;
+gPhotos[40].meta = new Object();
+
+gPhotos[41] = new Object();
+gPhotos[41].filename = "IMG_0207.jpg";
+gPhotos[41].ImageWidth = 450;
+gPhotos[41].ImageHeight = 600;
+gPhotos[41].ThumbWidth = 56;
+gPhotos[41].ThumbHeight = 75;
+gPhotos[41].meta = new Object();
+
